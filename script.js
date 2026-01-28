a3621af8c8c0952172dc384c93d08ca89b49aaaf
pedrojesusdev <pa04052007@gmail.com>
2026-01-28 17:15:22 -0300

    Adicionando produtos e smooth scroll

---
+++ script.js
@@ -0,0 +1,8 @@
+function smoothScrollTo(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.warn(`Element with ID '${elementId}' not found.`);
+    }
+}
